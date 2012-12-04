be9795fceaca6a9f79c58bc9816407ab4cb1e213
Alessandro Morandi <webmaster@simbul.net>
2012-12-04 20:04:59 +0000

    Added methods to improve testability.

--- lib/semaphoreapp/build.rb
+++ lib/semaphoreapp/build.rb
@@ -3,14 +3,25 @@ module Semaphoreapp
 
     def self.build(source)
       if source.is_a?(Hash)
-        Semaphoreapp::Build.new(
-          source.dup.tap do |hash|
-            hash['commit'] = Semaphoreapp::Commit.build(hash['commit'])
-          end
-        )
+        build_from_hash(source)
       elsif source.is_a?(Array)
-        source.map{ |build| Semaphoreapp::Build.build(build) }
+        build_from_array(source)
       end
+    end
+
+
+    private
+
+    def self.build_from_hash(build)
+      Semaphoreapp::Build.new(
+        build.dup.tap do |hash|
+          hash['commit'] = Semaphoreapp::Commit.build(hash['commit'])
+        end
+      )
+    end
+
+    def self.build_from_array(builds)
+      builds.map{ |build| Semaphoreapp::Build.build_from_hash(build) }
     end
 
   end
