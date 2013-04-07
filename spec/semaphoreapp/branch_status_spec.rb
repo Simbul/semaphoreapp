a3f034e7db215036619306c025af19c56e6f8121
Alessandro Morandi <webmaster@simbul.net>
2013-04-07 13:52:44 +0100

    Updated fixtures
    Contents come from the updated page on http://docs.semaphoreapp.com/api.

--- spec/semaphoreapp/branch_status_spec.rb
+++ spec/semaphoreapp/branch_status_spec.rb
@@ -6,7 +6,7 @@ describe Semaphoreapp::BranchStatus do
 
     context "with a hash" do
 
-      let(:test_hash){ fixture(:branch_statuses).first }
+      let(:test_hash){ fixture(:branch_status) }
       subject{ Semaphoreapp::BranchStatus.build(test_hash) }
 
       it "should call build_from_hash" do
@@ -16,28 +16,16 @@ describe Semaphoreapp::BranchStatus do
 
     end
 
-    context "with an array" do
-
-      let(:test_array){ fixture(:branch_statuses) }
-      subject{ Semaphoreapp::BranchStatus.build(test_array) }
-
-      it "should call build_from_array" do
-        Semaphoreapp::BranchStatus.should_receive(:build_from_array).with(test_array)
-        subject
-      end
-
-    end
-
   end
 
   describe ".build_from_hash" do
 
-    let(:test_hash){ fixture(:branch_statuses).first }
+    let(:test_hash){ fixture(:branch_status) }
     subject{ Semaphoreapp::BranchStatus.build_from_hash(test_hash) }
 
     it{ should be_an_instance_of Semaphoreapp::BranchStatus }
 
-    fixture(:branch_statuses).first.reject{ |key| key == 'commit' }.each do |key, value|
+    fixture(:branch_status).reject{ |key| key == 'commit' }.each do |key, value|
       it "should have attribute #{key}" do
         subject.send(key).should == value
       end
@@ -54,23 +42,6 @@ describe Semaphoreapp::BranchStatus do
       it "should set the commit attribute to nil" do
         subject.commit.should be_nil
       end
-    end
-
-  end
-
-  describe ".build_from_array" do
-
-    let(:test_array){ fixture(:branch_statuses) }
-    subject{ Semaphoreapp::BranchStatus.build_from_array(test_array) }
-
-    it{ should be_an_instance_of Array }
-
-    it "should call build_from_hash for all the hashes in the array" do
-      test_array.each do |test_hash|
-        Semaphoreapp::BranchStatus.should_receive(:build_from_hash).with(test_hash)
-      end
-
-      subject
     end
 
   end
